32cbe416e4ab6673ee50f51c787120ef32dc1545
Raj Kumar <raj.kumar@mjunction.in>
2024-07-18 00:47:54 +0530

    Searching

--- Algorithms/src/com/rj/Main.java
+++ Algorithms/src/com/rj/Main.java
@@ -2,6 +2,8 @@ package com.rj;
 
 import java.util.Arrays;
 
+import com.rj.searching.BinarySearch;
+import com.rj.searching.LinearSearch;
 import com.rj.sorting.BubbleSort;
 import com.rj.sorting.InsertionSort;
 import com.rj.sorting.MergeSort;
@@ -12,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {5,3,4,2,6,4,7,8,1};
+		int[] arr = {5,3,4,2,6,8,7,8,1};
 		
 //		BubbleSort.bubbleSort(arr);
 		
@@ -24,7 +26,13 @@ public class Main {
 		
 		MergeSort.mergeSort(0, arr.length-1, arr);
 		
-		System.out.println(Arrays.toString(arr));
+//		System.out.println(Arrays.toString(arr));
+		
+//		System.out.println(LinearSearch.searchOnArray(arr, 4));
+		
+//		System.out.println(BinarySearch.binarySearchRecurssive(arr, 0, arr.length-1, 4));
+		
+		System.out.println(BinarySearch.binarySearchIterative(arr, 8));
 		
 	}
 
